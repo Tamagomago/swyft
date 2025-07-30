4de930f0d87fa1720e03ff14416c3ebafd70ca91
Tamagomago <tamayoraeanchrissean@gmail.com>
2025-07-30 21:12:30 +0800

    feat: implement login form UI and logic

--- src/components/auth/LoginForm.tsx
+++ src/components/auth/LoginForm.tsx
@@ -1,15 +1,91 @@
-import AuthHeader from '@/components/ui/AuthHeader';
+'use client';
+
+import React from 'react';
+import AuthTitle from '@/components/ui/AuthTitle';
 import AuthDescription from '@/components/ui/AuthDescription';
 import AuthCard from '@/components/ui/AuthCard';
+import AuthHeader from '@/components/ui/AuthHeader';
+import AuthContent from '@/components/ui/AuthContent';
+import Label from '@/components/ui/Label';
+import Input from '@/components/ui/Input';
+import Button from '@/components/ui/Button';
+import { useRouter } from 'next/navigation';
+import { createClient } from '@/utils/supabase/client';
 
 function LoginForm() {
+  const [email, setEmail] = React.useState('');
+  const [password, setPassword] = React.useState('');
+  const [error, setError] = React.useState<string | null>(null);
+  const [isLoading, setIsLoading] = React.useState<boolean>(false);
+  const router = useRouter();
+
+  const handleLogin = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    const supabase = createClient();
+    setIsLoading(true);
+    setError(null);
+    try {
+      const { error } = await supabase.auth.signInWithPassword({
+        email,
+        password,
+      });
+
+      if (error) throw error;
+      console.log('Login successful');
+      router.push('/protected');
+    } catch (error: unknown) {
+      setError(error instanceof Error ? error.message : 'An error occurred.');
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
   return (
     <AuthCard>
-      {/* Headers */}
-      <div>
-        <AuthHeader>Login</AuthHeader>
-        <AuthDescription>Enter your email and password to login to your account.</AuthDescription>
-      </div>
+      <AuthHeader>
+        <AuthTitle>Login</AuthTitle>
+        <AuthDescription>Enter your email to login to your account.</AuthDescription>
+      </AuthHeader>
+      <form onSubmit={handleLogin}>
+        <AuthContent>
+          <div className={'w-full flex flex-col gap-2'}>
+            <Label htmlFor={'email'}>Email</Label>
+            <Input
+              className={`${error ? 'border-error bg-error/10! ring-error' : ''}`}
+              id={'email'}
+              type={'email'}
+              placeholder={'you@example.com'}
+              value={email}
+              required
+              onChange={(e: React.ChangeEvent<HTMLInputElement>) => setEmail(e.target.value)}
+            />
+          </div>
+          <div className={'w-full flex flex-col gap-2'}>
+            <Label htmlFor={'password'}>Password</Label>
+            <Input
+              className={`${error ? 'border-error bg-error/10! ring-error' : ''}`}
+              id={'password'}
+              type={'password'}
+              placeholder={'••••••••'}
+              required
+              value={password}
+              onChange={(e: React.ChangeEvent<HTMLInputElement>) => setPassword(e.target.value)}
+            />
+            {error && <p className={'text-sm text-error'}>{error}</p>}
+          </div>
+
+          {/* Login Button */}
+          <div className={'mt-7 w-full'}>
+            <Button
+              type={'submit'}
+              className={`bg-button-background w-full text-button-foreground ${isLoading ? 'cursor-not-allowed bg-button-hover! opacity-50' : ''}`}
+              disabled={isLoading}
+            >
+              {isLoading ? 'Logging in ...' : 'Login'}
+            </Button>
+          </div>
+        </AuthContent>
+      </form>
     </AuthCard>
   );
 }
