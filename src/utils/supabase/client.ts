3282fbe00aabde0db7c0f0c4b729df9ced77b7fb
Tamagomago <tamayoraeanchrissean@gmail.com>
2025-07-30 21:10:38 +0800

    refactor: use env.ts for environment variable access

--- src/utils/supabase/client.ts
+++ src/utils/supabase/client.ts
@@ -1,8 +1,6 @@
 import { createBrowserClient } from '@supabase/ssr';
+import { NEXT_PUBLIC_SUPABASE_URL, NEXT_PUBLIC_SUPABASE_ANON_KEY } from '../../../config/env';
 
 export function createClient() {
-  return createBrowserClient(
-    process.env.NEXT_PUBLIC_SUPABASE_URL!,
-    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
-  );
+  return createBrowserClient(NEXT_PUBLIC_SUPABASE_URL!, NEXT_PUBLIC_SUPABASE_ANON_KEY!);
 }
