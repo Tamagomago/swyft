93e2110d349c9092a8395e28c1a4847df7be00a3
Tamagomago <tamayoraeanchrissean@gmail.com>
2025-07-30 21:08:55 +0800

    refactor: rename AuthHeader -> AuthTitle

---
+++ src/components/ui/AuthTitle.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function AuthTitle({ children }: { children: React.ReactNode }) {
+  return <h1 className={'font-600 text-foreground pb-3 text-2xl sm:text-3xl'}>{children}</h1>;
+}
+
+export default AuthTitle;
