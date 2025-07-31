2adb12383aa59a21df9fd4d97d87782f54426514
Tamagomago <tamayoraeanchrissean@gmail.com>
2025-07-31 21:06:12 +0800

    style: change margin bottom value

--- src/components/ui/AuthHeader.tsx
+++ src/components/ui/AuthHeader.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
 function AuthHeader({ children }: { children: React.ReactNode }) {
-  return <header className={'mb-10'}>{children}</header>;
+  return <header className={'mb-7'}>{children}</header>;
 }
 
 export default AuthHeader;
