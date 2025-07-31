9dc89030831ff5f9e3400cdc2652d34c280c6f77
Tamagomago <tamayoraeanchrissean@gmail.com>
2025-07-31 21:06:32 +0800

    style: change button default font size

--- src/components/ui/Button.tsx
+++ src/components/ui/Button.tsx
@@ -7,7 +7,7 @@ interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
 function Button({ children, className = '', type, disabled }: ButtonProps) {
   return (
     <button
-      className={`${className} focus:outline-none py-2 px-3 hover:bg-button-hover  text-sm placeholder-muted border-2 focus:ring-2 focus:ring-ring rounded-md`}
+      className={`${className} focus:outline-none py-2 px-3 hover:bg-button-hover placeholder-muted border-2 focus:ring-2 focus:ring-ring rounded-md`}
       disabled={disabled || false}
       type={type || 'button'}
     >
