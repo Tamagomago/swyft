6868acb6716b9b6213671707570677f19b45705d
Tamagomago <tamayoraeanchrissean@gmail.com>
2025-08-01 21:12:19 +0800

    feat: switch from OTP verification to code-based session exchange

--- src/app/auth/confirm/route.ts
+++ src/app/auth/confirm/route.ts
@@ -1,22 +1,16 @@
 import { createClient } from '@/utils/supabase/server';
-import { type EmailOtpType } from '@supabase/supabase-js';
 import { redirect } from 'next/navigation';
 import { type NextRequest } from 'next/server';
 
 export async function GET(request: NextRequest) {
   const { searchParams } = new URL(request.url);
-  const token_hash = searchParams.get('token_hash');
-  const type = searchParams.get('type') as EmailOtpType | null;
+  const code = searchParams.get('code');
   const _next = searchParams.get('next');
   const next = _next?.startsWith('/') ? _next : '/';
 
-  if (token_hash && type) {
+  if (code) {
     const supabase = await createClient();
-
-    const { error } = await supabase.auth.verifyOtp({
-      type,
-      token_hash,
-    });
+    const { error } = await supabase.auth.exchangeCodeForSession(code);
     if (!error) {
       // redirect user to specified redirect URL or root of app
       redirect(next);
