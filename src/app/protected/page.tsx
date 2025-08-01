1afd706ecfe555ff74afab42e683dc3e02f5131d
Tamagomago <tamayoraeanchrissean@gmail.com>
2025-08-01 21:13:11 +0800

    chore: change bg style for testing

--- src/app/protected/page.tsx
+++ src/app/protected/page.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 function TestPage() {
   console.log('protected page');
-  return <div className={'text-foreground'}>Hello from protected page</div>;
+  return <div className={'text-foreground w-full bg-background'}>Hello from protected page</div>;
 }
 
 export default TestPage;
