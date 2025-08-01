501a9d9fdffd922e6e29b3f7405e84c992aa8722
Tamagomago <tamayoraeanchrissean@gmail.com>
2025-08-01 21:08:41 +0800

    feat(auth): add sign up success page

---
+++ src/app/auth/sign-up-success/page.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import AuthCard from '@/components/ui/auth-card';
+import AuthTitle from '@/components/ui/auth-title';
+
+function Page() {
+  return (
+    <div className={' flex h-full items-center justify-center font-600 text-foreground'}>
+      <AuthCard>
+        <AuthTitle>Thank you for signing up!</AuthTitle>
+        <div className={'mt-3'}>
+          <p className={'text-sm text-foreground font-sm'}>
+            You have successfully signed up. Please check your email to confirm before signing in.
+          </p>
+        </div>
+      </AuthCard>
+    </div>
+  );
+}
+
+export default Page;
