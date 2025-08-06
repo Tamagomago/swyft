d0753ec59495ef1214b51195782b659ccaf96e55
Tamagomago <tamayoraeanchrissean@gmail.com>
2025-08-06 21:23:24 +0800

    feat(navbar): add hamburger toggle component to navbar

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,5 +1,6 @@
-import ThemeToggle from './theme-toggle';
+import ThemeToggle from './navbar/theme-toggle';
 import Link from 'next/link';
+import HamburgerToggle from '@/components/navbar/hamburger-toggle';
 
 function Navbar() {
   return (
@@ -8,6 +9,7 @@ function Navbar() {
         'z-1 flex items-center justify-between w-[95%] py-5 px-3 bg-transparent absolute top-0 text-foreground'
       }
     >
+      <HamburgerToggle className={'md:hidden'} />
       <Link href={'/home'}>
         <h1 className={'text-ctm-foreground text-2xl font-bold'}>Swyft</h1>
       </Link>
---
+++ src/components/navbar/hamburger-toggle.tsx
@@ -0,0 +1,35 @@
+'use client';
+
+import { RiMenuFill } from 'react-icons/ri';
+import { RiCloseFill } from 'react-icons/ri';
+import React from 'react';
+import Button from '@/components/ui/button';
+import { useSidebarStore } from '@/store/sidebar';
+
+interface HamburgerToggleProps {
+  className?: string;
+}
+
+function HamburgerToggle({ className }: HamburgerToggleProps) {
+  const { isOpen, toggle } = useSidebarStore();
+
+  return (
+    <Button
+      className={`${className} bg-transparent hover:bg-transparent border-none ring-0 focus:outline-none! focus:ring-0! aspect-square`}
+      onClick={toggle}
+    >
+      <span className={'text-foreground relative flex items-center justify-center aspect-square'}>
+        <RiMenuFill
+          className={`absolute aspect-square transition-transform duration-300 ${isOpen ? 'scale-0 rotate-180' : 'scale-100 rotate-180'} `}
+          size={20}
+        />
+        <RiCloseFill
+          className={`absolute aspect-square transition-transform duration-300 ${!isOpen ? 'scale-0 rotate-180' : 'scale-100 rotate-180'} `}
+          size={20}
+        />
+      </span>
+    </Button>
+  );
+}
+
+export default HamburgerToggle;
