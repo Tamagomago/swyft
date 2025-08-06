a72885963fbab9007c1a506f0c7688f1b16a9f22
Tamagomago <tamayoraeanchrissean@gmail.com>
2025-08-06 21:22:54 +0800

    feat(controls): add delete icon to controls

--- src/components/sidebar/controls.tsx
+++ src/components/sidebar/controls.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { RiStickyNoteAddLine } from 'react-icons/ri';
 import { RiFolderAddLine } from 'react-icons/ri';
+import { RiDeleteBin6Line } from 'react-icons/ri';
 
 function Controls() {
   return (
@@ -9,6 +10,7 @@ function Controls() {
       <div className={'flex lg:gap-2 gap-1.5 px-2 items-center justify-center'}>
         <RiStickyNoteAddLine className={'text-muted hover:text-foreground'} size={17} />
         <RiFolderAddLine className={'text-muted hover:text-foreground'} size={17} />
+        <RiDeleteBin6Line className={'text-muted hover:text-foreground'} size={17} />
       </div>
     </div>
   );
