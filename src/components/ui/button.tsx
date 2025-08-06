f782a7de2d12d7d012acbf0777fae0327848ab3d
Tamagomago <tamayoraeanchrissean@gmail.com>
2025-08-06 08:36:46 +0800

    chore(button): add onClick parameter

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -4,12 +4,13 @@ interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
   children?: React.ReactNode;
   className?: string;
 }
-function Button({ children, className = '', type, disabled }: ButtonProps) {
+function Button({ children, className = '', type, disabled, onClick }: ButtonProps) {
   return (
     <button
       className={`${className} focus:outline-none py-2 px-3 hover:bg-button-hover placeholder-muted border-2 focus:ring-2 focus:ring-ring rounded-md`}
       disabled={disabled || false}
       type={type || 'button'}
+      onClick={onClick}
     >
       {children}
     </button>
