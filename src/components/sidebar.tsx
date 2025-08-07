e8f4bfc1ee300a22887d44d721b1c8f24c3a5455
Tamagomago <tamayoraeanchrissean@gmail.com>
2025-08-07 20:05:13 +0800

    feat(sidebar): add close functionality to sidebar on outside click

--- src/components/sidebar.tsx
+++ src/components/sidebar.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import React from 'react';
+import React, { useEffect } from 'react';
 import Controls from '@/components/sidebar/controls';
 import NotesList from '@/components/sidebar/notes-list';
 import { useSidebarStore } from '@/store/sidebar';
@@ -10,17 +10,30 @@ interface SidebarProps {
 }
 
 function Sidebar({ className }: SidebarProps) {
-  const { isOpen } = useSidebarStore();
+  const { isOpen, close } = useSidebarStore();
+  let sidebarRef = React.useRef<HTMLDivElement>(null);
 
+  useEffect(() => {
+    console.log('from sidebar', isOpen);
+    let handler = (e: MouseEvent | TouchEvent) => {
+      if (sidebarRef.current && !sidebarRef.current.contains(e.target as Node) && isOpen) {
+        close();
+      }
+    };
+
+    document.addEventListener('mousedown', handler);
+    return () => document.removeEventListener('mousedown', handler);
+  }, [isOpen]);
   return (
     <div
       className={`
         ${className}
-        flex flex-col gap-5 h-full w-full
+        flex flex-col gap-5 h-full w-[80%]
         pt-25 px-10 bg-sidebar-background
-        transform transition-transform duration-300 ease-in-out
+        transform transition-transform duration-200 ease-in-out
         ${isOpen ? 'translate-x-0' : '-translate-x-full md:translate-x-0'}
       `}
+      ref={sidebarRef}
     >
       <Controls />
       <NotesList />
--- src/store/sidebar.ts
+++ src/store/sidebar.ts
@@ -4,9 +4,11 @@ interface SidebarState {
   isOpen: boolean;
   toggle: () => void;
   open: () => void;
+  close: () => void;
 }
 export const useSidebarStore = create<SidebarState>((set) => ({
   isOpen: false,
   toggle: () => set((state) => ({ isOpen: !state.isOpen })),
   open: () => set({ isOpen: true }),
+  close: () => set({ isOpen: false }),
 }));
