ef39208298aa74fe24d3c54deafb1b8c73e1f117
Tamagomago <tamayoraeanchrissean@gmail.com>
2025-08-07 20:04:47 +0800

    fix(hamburger toggle): remove close icon

--- src/components/navbar/hamburger-toggle.tsx
+++ src/components/navbar/hamburger-toggle.tsx
@@ -1,8 +1,7 @@
 'use client';
 
-import { RiMenuFill } from 'react-icons/ri';
-import { RiCloseFill } from 'react-icons/ri';
-import React from 'react';
+import { RiSideBarLine } from 'react-icons/ri';
+import React, { useEffect } from 'react';
 import Button from '@/components/ui/button';
 import { useSidebarStore } from '@/store/sidebar';
 
@@ -15,16 +14,12 @@ function HamburgerToggle({ className }: HamburgerToggleProps) {
 
   return (
     <Button
-      className={`${className} bg-transparent hover:bg-transparent border-none ring-0 focus:outline-none! focus:ring-0! aspect-square`}
+      className={`${className} bg-transparent hover:bg-transparent border-none ring-0 focus:!outline-none focus:!ring-0 aspect-square`}
       onClick={toggle}
     >
       <span className={'text-foreground relative flex items-center justify-center aspect-square'}>
-        <RiMenuFill
-          className={`absolute aspect-square transition-transform duration-300 ${isOpen ? 'scale-0 rotate-180' : 'scale-100 rotate-0'} `}
-          size={20}
-        />
-        <RiCloseFill
-          className={`absolute aspect-square transition-transform duration-300 ${!isOpen ? 'scale-0 rotate-180' : 'scale-100 rotate-0'} `}
+        <RiSideBarLine
+          className={`absolute aspect-square transition-transform duration-300 ${isOpen ? 'scale-0 rotate-180  hidden' : 'scale-100 rotate-0'} `}
           size={20}
         />
       </span>
