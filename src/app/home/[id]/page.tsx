231c99cb1e2ef6950147968985d016898bef0b96
Tamagomago <tamayoraeanchrissean@gmail.com>
2025-08-08 19:53:37 +0800

    fix(page): await params to extract id correctly

--- src/app/home/[id]/page.tsx
+++ src/app/home/[id]/page.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
 
-function Page({ params }: { params: { id: string } }) {
+async function Page({ params }: { params: Promise<{ id: string }> }) {
+  const { id } = await params;
   return (
     <div className={'text-foreground w-full h-full'}>
       <h1>Hello from Notes Page</h1>
-      <h1>Test params: {params.id}</h1>
+      <h1>Test params: {id}</h1>
     </div>
   );
 }
