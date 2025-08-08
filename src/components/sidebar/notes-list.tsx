b9e55961b09b7c053883f7ba4d04104c24a3f6dc
Tamagomago <tamayoraeanchrissean@gmail.com>
2025-08-08 21:34:47 +0800

    feat(sidebar): implement loading skeleton and note selection functionality

--- src/components/sidebar/notes-list.tsx
+++ src/components/sidebar/notes-list.tsx
@@ -3,11 +3,15 @@ import React, { useEffect, useState } from 'react';
 import { Notes } from '../../../types/supabase';
 import { PostgrestError } from '@supabase/supabase-js';
 import { getUserNotes } from '@/lib/notes';
+import SkeletonList from '@/components/ui/skeleton-list';
+import { useRouter } from 'next/navigation';
 
 function NotesList() {
   const [data, setData] = useState<Notes[] | null>(null);
   const [error, setError] = useState<string | PostgrestError | null>(null);
   const [isLoading, setIsLoading] = useState<boolean>(true);
+  const [selectedNote, setSelectedNote] = useState<string>('');
+  const router = useRouter();
 
   useEffect(() => {
     (async () => {
@@ -25,15 +29,26 @@ function NotesList() {
     })();
   }, [data]);
   return (
-    <div className={'text-foreground'}>
+    <div className={'font-medium text-sm text-muted'}>
       {isLoading ? (
-        <p>Loading notes...</p>
+        <SkeletonList count={7} />
       ) : error ? (
-        <p className={'text-error'}>{error as string}</p>
+        <p className={'text-muted'}>{error as string}</p>
       ) : data && data.length > 0 ? (
         <ul>
           {data.map((note) => (
-            <li key={note.id} className={'mb-2'}>
+            <li
+              key={note.id}
+              className={`
+                hover:bg-muted/20 py-1 px-1.5 rounded-md cursor-pointer 
+                transition-all hover:font-bold hover:scale-102 truncate
+                ${selectedNote === note.id ? 'bg-muted/20 font-bold' : ''}
+                `}
+              onClick={() => {
+                setSelectedNote(note.id);
+                router.push(`/home/${note.id}`);
+              }}
+            >
               {note.title}: {note.content}
             </li>
           ))}
