d9cf34e82a5316596b42436e3692530df71d0aba
Tamagomago <tamayoraeanchrissean@gmail.com>
2025-08-12 13:15:58 +0800

    feat(notes db): implement create notes functionality

--- src/lib/notes.ts
+++ src/lib/notes.ts
@@ -1,19 +1,31 @@
 import { createClient } from '@/utils/supabase/client';
-import { Notes } from '../../types/supabase';
+import { Notes } from '@/types/supabase';
 import { PostgrestError } from '@supabase/supabase-js';
+
+const supabase = createClient();
+const {
+  data: { user },
+} = await supabase.auth.getUser();
 
 export async function getUserNotes(): Promise<{
   data: Notes[] | null;
   error: PostgrestError | null | string;
 }> {
-  const supabase = createClient();
-  const {
-    data: { user },
-  } = await supabase.auth.getUser();
-
   if (!user) throw new Error('User not authenticated');
 
   const { data, error } = await supabase.from('notes').select('*').eq('user_id', user.id);
 
+  return { data, error };
+}
+
+export async function createNote({
+  title,
+  content = '',
+}: Notes): Promise<{ data: Notes; error: PostgrestError | string | null }> {
+  const { data, error } = await supabase
+    .from('notes')
+    .insert({ title, content, user_id: user?.id })
+    .select()
+    .single();
   return { data, error };
 }
