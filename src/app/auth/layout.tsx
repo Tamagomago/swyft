686ca61a5722a13333430e13ef01ef998fa55600
Tamagomago <tamayoraeanchrissean@gmail.com>
2025-08-15 20:19:30 +0800

    style: update theme variables and layout styles

--- src/app/auth/layout.tsx
+++ src/app/auth/layout.tsx
@@ -3,7 +3,9 @@ import React from 'react';
 function AuthLayout({ children }: { children: React.ReactNode }) {
   return (
     <div className="flex items-center justify-center md:grid md:grid-cols-3  w-screen  h-screen bg-background">
-      <div className={'md:col-span-1 h-full w-full md:border-r-2 md:border-border'}>{children}</div>
+      <div className={'md:col-span-1 h-full w-full md:border-r-2 md:border-border/30'}>
+        {children}
+      </div>
       <div className="col-span-2 h-full hidden md:block">
         <h1 className={'text-foreground'}>PLACEHOLDER</h1>
       </div>
--- src/app/home/layout.tsx
+++ src/app/home/layout.tsx
@@ -4,7 +4,7 @@ import Sidebar from '@/components/sidebar';
 function Layout({ children }: { children: React.ReactNode }) {
   return (
     <div className="bg-background md:grid md:grid-cols-5 w-full min-h-screen overflow-hidden">
-      <Sidebar className="md:col-span-1 fixed md:relative" />
+      <Sidebar className="lg:col-span-1 fixed lg:relative" />
       <div className="col-span-4 min-h-screen w-full flex justify-center items-center">
         {children}
       </div>
